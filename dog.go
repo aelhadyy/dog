89d964f69a012e3b6fdc9b51966dcb6c7ec1d6a6
Ahmed Elhady <ahmedelhady.dev@gmail.com>
2025-03-21 20:09:44 +0200

    Updated dog main

--- dog.go
+++ dog.go
@@ -1,5 +1,7 @@
 package dog
 
+import "strings"
+
 func WhenGrownUp(s string) string {
-	return "When the puppy grows up it says: " + s
+	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
